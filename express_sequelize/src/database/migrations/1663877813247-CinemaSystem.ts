223cde7b9b0c5d8e3e0331cb1f0cf7b54b1a55f9
sameerbinraees <sameer.raees@packagex.io>
2022-10-12 22:55:40 +0500

    creating migration

--- express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
+++ express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -1,4 +1,4 @@
-import { QueryInterface } from 'sequelize';
+import { QueryInterface, DataTypes } from 'sequelize';
 
 export default {
   /**
@@ -8,6 +8,14 @@ export default {
    To not introduce additional complexity, please consider only one cinema.
 
    Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
+
+   - movies -> id, showroomId (foreign key for the showroom), isScreening, name, screeningStartTime, screeningEndTime, bookedOut, pricePerSeat
+   - showrooms -> id, name, seatsAvailable
+   - seats -> id, showroomId (foreign key for the showroom), name, code, bookedOut
+   - seatTypes -> id, type, percentagePremium
+   - bookings -> id, movieId, seatId, showroomId, userId, active
+   - users -> id, email, name, phoneNumber
+   - roles -> id, roleType
 
    ## User Stories
 
@@ -31,8 +39,222 @@ export default {
    * As a cinema owner I don't want to configure the seating for every show
    */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  up: (queryInterface: QueryInterface): Promise<void> => {
-    throw new Error('TODO: implement migration in task 4');
+  up: async (queryInterface: QueryInterface): Promise<void> => {
+    await queryInterface.createTable('roles', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      roleType: { type: DataTypes.ENUM('admin', 'user'), allowNull: false, defaultValue: 'user' },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      roleId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'roles',
+          },
+          key: 'id',
+        },
+      },
+      email: { type: DataTypes.STRING(50), allowNull: false, unique: true },
+      name: { type: DataTypes.TEXT, allowNull: false },
+      phoneNumber: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+
+    await queryInterface.createTable('showrooms', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: { type: DataTypes.TEXT },
+      seatsAvailable: { type: DataTypes.BOOLEAN },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+
+    await queryInterface.createTable('movies', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: { type: DataTypes.TEXT, allowNull: false },
+      screeningStartTime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+      screeningEndTime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+      bookedOut: { type: DataTypes.BOOLEAN },
+      isScreening: { type: DataTypes.BOOLEAN },
+      showroomId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'showrooms',
+          },
+          key: 'id',
+        },
+      },
+      pricePerSeat: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+
+    await queryInterface.createTable('seatTypes', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      type: { type: DataTypes.TEXT, allowNull: false },
+      percentagePremium: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+
+    await queryInterface.createTable('seats', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: { type: DataTypes.TEXT },
+      code: { type: DataTypes.TEXT, allowNull: false }, // seat number and row
+      bookedOut: { type: DataTypes.BOOLEAN },
+      showroomId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'showrooms',
+          },
+          key: 'id',
+        },
+      },
+      seatTypeId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'seatTypes',
+          },
+          key: 'id',
+        },
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+
+    await queryInterface.createTable('bookings', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      movieId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'movies',
+          },
+          key: 'id',
+        },
+      },
+      seatId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'seats',
+          },
+          key: 'id',
+        },
+      },
+      showroomId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'showrooms',
+          },
+          key: 'id',
+        },
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'users',
+          },
+          key: 'id',
+        },
+      },
+      active: { type: DataTypes.BOOLEAN },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
